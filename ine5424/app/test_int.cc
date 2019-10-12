7e01c5dc2d868869b20ba160355aaacf23ae9b26
AdrianoTosetto <adriano.rafael10@hotmail.com>
2019-10-11 23:35:12 -0300

    add interrupts test. Not working yet

---
+++ ine5424/app/test_int.cc
@@ -0,0 +1,60 @@
+#include <machine/nic.h>
+#include <time.h>
+#include <real-time.h>
+#include <utility/ostream.h>
+#include <synchronizer.h>
+
+
+using namespace EPOS;
+
+OStream cout;
+
+int test_int() {
+    TSC_Chronometer c;
+    c.start();
+    cout << "NIC Test" << endl;
+
+    NIC<Ethernet> * nic = Traits<Ethernet>::DEVICES::Get<0>::Result::get(0);
+
+    NIC<Ethernet>::Address src, dst;
+    NIC<Ethernet>::Protocol prot;
+    char data[nic->mtu()+1];
+
+    NIC<Ethernet>::Address self = nic->address();
+    cout << "  MAC: " << self << endl;
+
+    if(self[5] % 2) { // sender
+        Delay (5000000);
+
+        for(int i = 0; i < 1000; i++) {
+            memset(data, '1' + i, nic->mtu()+1);
+            data[nic->mtu() - 1] = '\n';
+            nic->send(nic->broadcast(), 0x8888, data, nic->mtu());
+        }
+    } else {
+        TSC_Chronometer cr;
+
+           nic->receive(&src, &prot, data, nic->mtu());
+           cout << "  Data: " << data;
+        
+    }
+
+    NIC<Ethernet>::Statistics stat = nic->statistics();
+    cout << "Statistics\n"
+         << "Tx Packets: " << stat.tx_packets << "\n"
+         << "Tx Bytes:   " << stat.tx_bytes << "\n"
+         << "Rx Packets: " << stat.rx_packets << "\n"
+         << "Rx Bytes:   " << stat.rx_bytes << "\n";
+    c.stop();
+    return c.read();
+}
+
+int main()
+{
+    int ttest_one = test_one() / 1000000;
+    int ttest_two = test_two() / 1000000;
+    int ttest_three = test_three() / 1000000;
+    cout << "Teste 1 demorou " << ttest_one << " segundos" << endl;
+    cout << "Teste 2 demorou " << ttest_two << " segundos" << endl;
+    cout << "Teste 3 demorou " << ttest_three << " segundos" << endl;
+}
