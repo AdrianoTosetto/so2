ab1b3e9d95dfad03edf7dc009c06558ac427304d
AdrianoTosetto <adriano.rafael10@hotmail.com>
2019-10-24 18:11:29 -0300

    add send1 and receive1 to test header

--- ine5424/include/network/bolinha_protocol.h
+++ ine5424/include/network/bolinha_protocol.h
@@ -15,13 +15,15 @@ __BEGIN_SYS
 Semaphore *sem;
 
 void timeout() {
-    db<Thread>(WRN) << "TIMEOUT FUNCAO" << endl;
+    db<Thread>(WRN) << "Ocorreu um timeout" << endl;
     (*sem).v();
 }
-void polling() {
-    db<Thread>(WRN) << "Fazendo Polling" << endl;
-    (*sem).v();
-}
+
+struct messages {
+    int _status;
+    int _id;
+};
+typedef struct messages messages;
 
 class Bolinha_Protocol: private NIC<Ethernet>::Observer, Concurrent_Observer<Ethernet::Buffer, Ethernet::Protocol>
 {
@@ -39,15 +41,26 @@ public:
     }
     int send(const void *data, size_t size) {
         int bytes;// = _nic->send(_nic->broadcast(), Prot_Bolinha, data, size);
-        int n = 3;
+        int n = 3; // numero de tentativas 
         sem = &_sem;
         Function_Handler handler_a(&timeout);
-        Function_Handler handler_b(&polling);
-        Alarm *alarm_a = new Alarm(2000000, &handler_a, 10000);
-        //Alarm alarm_b(200, &handler_b, 10000);
+        Alarm *alarm_a = new Alarm(2000000, &hand```
+        typedef unsigned char Data[Bolinha_MTU];
+        Buffer *buf = _nic->alloc(to, Prot_Bolinha, 0, sizeof(Header), size+sizeof(Header));
+        if(!buf) {
+            db<Thread>(WRN) << "Nao alocado " << addr() << endl;
+            return 0;
+        }
+
+        /*Frame * frame = buf->frame()->data<Frame>();
+        size_t s = (size >= sizeof(Frame)) ? sizeof(Frame) : size;
+        memcpy(frame, data, size);*/
+        db<Thread>(WRN) << "quem enviou " << addr() << "\n"
+                        << "quem recebeu " << to << "\n"
+                        << "dado enviado " << data << endl;
+        _nic->send(to, Prot_Bolinha, data, size);```ler_a, 10000);
         while(!this->_status && n > 0) {
             bytes = _nic->send(_nic->broadcast(), Prot_Bolinha, data, size);
-            db<Thread>(WRN) << "antes do sem" << endl;
             (*sem).p();
             n--;
         }
@@ -58,35 +71,52 @@ public:
             db<Thread>(WRN) << "timed out" << endl;
             bytes = 0;
         }
-        //db<Thread>(WRN) << "depois do while" << endl;
         return bytes;
     }
     int receive(void *buffer, size_t size) {
         Buffer *rec = updated();
         memcpy(buffer, rec->frame()->data<char>(), size);
-        char *test = reinterpret_cast<char*>(buffer);
-        if(test[0] == 'A') {
-            db<Thread>(WRN) << "Chegou um ACK" << endl;
-            this->_status = 1;
-            (*sem).v();
-        } else {
-            char ack[2];
-            ack[0] = 'A';
-            ack[1] = '\n';
-            Delay(20000000);
-            db<Thread>(WRN) << "Mandando um ACK" << endl;
-            int bytes = _nic->send(_nic->broadcast(), Prot_Bolinha, ack, size);
+
+        char ack[2];
+        ack[0] = 'A'; // nosso ACK por agora é um char com c[0] = 'A'
+        ack[1] = '\n';
+        //Delay(20000000); // forçando um timeout
+        db<Thread>(WRN) << "Mandando um ACK" << endl;
+        int bytes = _nic->send(_nic->broadcast(), Prot_Bolinha, ack, size);
+        
+        _nic->free(rec);
+        return size;
+    }
+    int send1(const void *data, size_t size, Address& to) {
+        //Frame f();
+        typedef unsigned char Data[Bolinha_MTU];
+        Buffer *buf = _nic->alloc(to, Prot_Bolinha, 0, sizeof(Header), size+sizeof(Header));
+        if(!buf) {
+            db<Thread>(WRN) << "Nao alocado " << addr() << endl;
+            return 0;
         }
+
+        /*Frame * frame = buf->frame()->data<Frame>();
+        size_t s = (size >= sizeof(Frame)) ? sizeof(Frame) : size;
+        memcpy(frame, data, size);*/
+        db<Thread>(WRN) << "quem enviou " << addr() << "\n"
+                        << "quem recebeu " << to << "\n"
+                        << "dado enviado " << data << endl;
+        _nic->send(to, Prot_Bolinha, data, size);
+    }
+    int receive1(void *buffer, size_t size) {
+        Buffer *rec = updated();
+        memcpy(buffer, rec->frame()->data<char>(), size);
         _nic->free(rec);
         return size;
     }
     static bool notify(const Protocol& p, Buffer *b) {
-        db<Thread>(WRN) << "notified" << endl;
         return _observed.notify(p, b);
     }
     void update(Observed *o, const Protocol& p, Buffer *b) {
+        db<Thread>(WRN) << "passando pelo update do endereco " << addr() << endl;
         char *data = reinterpret_cast<char*>(b->frame()->data<char>());
-        if (data[0] == 'A') {
+        if (data[0] == 'A') { // Quando ocorre um timeout o proprio nodo que enviou o ACK printa "ACK Recebido"
             db<Thread>(WRN) << "ACK recebido" << endl;
             _status = 1;
             (*sem).v();
@@ -101,22 +131,28 @@ public:
     }
     class Header {
     public:    
-        Address _to;
+        //Address _to;
         Address _from;
-        int unsigned short  _flags: 1; // por agora temos só a flag isACK, se houver necessidade de mais flags
-                                      // aumentar o campo de bits desse atributo _flags
-        unsigned short  _checksum; // verificar se o frame chegou com todos os bits corretos
-        unsigned short  _length;
+        bool  _flags; // ACK
         unsigned short  _id;
+        //unsigned short  _checksum; // verificar se o frame chegou com todos os bits corretos
+        //unsigned short  _length;
 
+        Header(Address from, unsigned short id): 
+        _id(id), _from(from), _flags(false)
+        {}
+        void flags(bool ack) {
+            _flags = ack;
+        }
     } __attribute__((packed));
 
     class Frame: private Header {
     public: 
-        Frame() {
-
-        }
+        Frame(Address to, Address from, unsigned short id, const void* data, size_t len): 
+            Header(from, id), _data(data), _len(len) {}
         typedef unsigned char Data[];
+        const void* _data;
+        size_t _len;
     } __attribute__((packed));
 protected:
     int _status;
@@ -126,6 +162,8 @@ protected:
     static Observed _observed;
     static const unsigned int NIC_MTU = 1500;
     static const unsigned int Bolinha_MTU = NIC_MTU - sizeof(Header);
+    static messages _messages[1000];
+
 };
 
 
