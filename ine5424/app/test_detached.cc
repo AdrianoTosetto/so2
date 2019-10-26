c91ff915ec89cb3934aeb193b54482d01df75b49
gstvob <gustavo.borgesfr@gmail.com>
2019-10-26 20:24:49 -0300

    Adding initial port tests

--- ine5424/app/test_detached.cc
+++ ine5424/app/test_detached.cc
@@ -6,30 +6,44 @@ typedef Ethernet::Address Address;
 
 OStream cout;
 
+
+NIC<Ethernet> *nic = Traits<Ethernet>::DEVICES::Get<0>::Result::get(0);
+char data[1500];
+
+int sender(short port) {
+    Bolinha_Protocol *bp = new Bolinha_Protocol(port);
+    Delay (5000000);
+    char *hello;
+    if (port == 6000) {
+       hello = "Hello\n";
+    } else {
+        hello = "olleH\n";
+    }
+    Address d = bp->addr();
+    d[5]--;
+    cout << "Dado enviado: " << hello << ", para a porta: "<< port << endl;
+    return bp->send(hello, 1500, d, port);
+}
+
+int receiver(short port) {
+    Bolinha_Protocol *bp = new Bolinha_Protocol(port);
+    bp->receive(data, 1500);
+    cout << "Dado recebido: " << data << endl;
+    return 1;
+}
+
+
 int main()
 {
+    
     cout << "NIC Test" << endl;
-    NIC<Ethernet> *nic = Traits<Ethernet>::DEVICES::Get<0>::Result::get(0);
     cout << "Meu endereco eh " << nic->address() << endl;
-    char data[1500];
     if(nic->address()[5] % 2) { // sender
-        Bolinha_Protocol *bp = new Bolinha_Protocol(5000);
-        Delay (5000000);
-        data[0] = 'H';
-        data[1] = 'e';
-        data[2] = 'l';
-        data[3] = 'l';
-        data[4] = 'o';
-        data[5] = '\n';
-        char* hello = "Hello\n";
-        Address d = bp->addr();
-        d[5]--;
-        cout << "Dado enviado: " << hello << endl;
-        bp->send(hello, 1500, d, 5001);
+        sender(5000);
+        sender(6000);
     } else {
-        Bolinha_Protocol *bp = new Bolinha_Protocol(5001);
-        bp->receive(data, 1500);
-        cout << "Dado recebido: " << data << endl;
+       receiver(5000);
+       receiver(6000);
     }
 
     /*NIC<Ethernet>::Statistics stat = sp->nic()->statistics();
