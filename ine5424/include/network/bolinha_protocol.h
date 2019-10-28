35bfd6be610d2665681d2d87fd9c0f2a69a10325
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2019-10-28 16:38:03 -0300

    add sequential ids

--- ine5424/include/network/bolinha_protocol.h
+++ ine5424/include/network/bolinha_protocol.h
@@ -9,8 +9,19 @@
 #include <utility/bitmap.h>
 #include <machine/nic.h>
 #include <time.h>
+#include <queue.h>
+
 
 __BEGIN_SYS
+
+struct Frame_Track {
+    short _frame_id;
+    short _port;
+
+    Frame_Track(short frame_id, short port): _frame_id(frame_id), _port(port) {}
+};
+
+typedef struct Frame_Track FT;
 
 class Bolinha_Protocol: private NIC<Ethernet>::Observer, Concurrent_Observer<Ethernet::Buffer, Ethernet::Protocol>
 {
@@ -46,7 +57,7 @@ public:
             Semaphore_Handler handler_a(&sem);
             Alarm alarm_a = Alarm(2*1000000, &handler_a, n);
 
-            Frame *f = new Frame(to, addr(), &status, data, _using_port, port_receiver, 5);
+            Frame *f = new Frame(to, addr(), CPU::finc<short int>(_packet_count), &status, data, _using_port, port_receiver, 5);
             f->sem(&sem);
 
             while(!status && n > 0) {
@@ -72,7 +83,7 @@ public:
         memcpy(buffer, f->data<char>(), size);
 
         char* ack_data = (char*) "ACK\n";
-        Frame *ack = new Frame(f->from(), addr(), f->id(), ack_data, _using_port, f->port_sender(), 0);
+        Frame *ack = new Frame(f->from(), addr(), -1, f->status(), ack_data, _using_port, f->port_sender(), 0);
         ack->flags(true);
         ack->sem(f->sem());
         _nic->send(f->from(), Prot_Bolinha, ack, size);
@@ -91,11 +102,11 @@ public:
             return;
         }
         if (f->flags()) {
-            db<Thread>(WRN) << "ACK " << f->id() << " recebido" << endl;
-            if (!CPU::tsl<bool>(*(f->id())))
+            db<Thread>(WRN) << "ACK " << f->frame_id() << " recebido" << endl;
+            if (!CPU::tsl<bool>(*(f->status())))
                 f->sem()->v();
             else
-                CPU::fdec<bool>(*(f->id()));
+                CPU::fdec<bool>(*(f->status()));
             delete f;
         }
         Concurrent_Observer<Observer::Observed_Data, Protocol>::update(p, b);
@@ -109,8 +120,8 @@ public:
     class Header {
     public:    
 
-        Header(Address from, bool* id, short port_sender, short port_receiver): 
-        _from(from), _id(id), _flags(false), _port_sender(port_sender), _port_receiver(port_receiver)
+        Header(Address from, short frame_id, bool* status, short port_sender, short port_receiver): 
+        _from(from), _frame_id(frame_id), _status(status), _flags(false), _port_sender(port_sender), _port_receiver(port_receiver)
         {}
 
         void flags(bool ack) {
@@ -119,8 +130,8 @@ public:
         void sem(Semaphore * sem) {
             _sem = sem;
         }
-        bool* id() {
-            return _id;
+        bool* status() {
+            return _status;
         }
         Address from() {
             return _from;
@@ -132,21 +143,22 @@ public:
             return _sem;
         }
         //Address _to;
-        Address _from;
-        bool*  _id;
-        bool  _flags; // ACK
         //unsigned short  _checksum; // verificar se o frame chegou com todos os bits corretos
         //unsigned short  _length;
     protected:
+        Address _from;
         Semaphore * _sem;
+        short _frame_id;
+        bool*  _status;
+        bool  _flags; // ACK
         short _port_sender;
         short _port_receiver;
     } __attribute__((packed));
 
     class Frame: private Header {
     public: 
-        Frame(Address to, Address from, bool* id, void* data, short port_sender, short port_receiver,size_t len): 
-            Header(from, id, port_sender, port_receiver), _len(len), _data(data) {}
+        Frame(Address to, Address from, int packet_id, bool* status, void* data, short port_sender, short port_receiver,size_t len): 
+            Header(from, packet_id, status, port_sender, port_receiver), _len(len), _data(data) {}
         typedef unsigned char Data[];
         size_t _len;
         void* _data;
@@ -157,8 +169,14 @@ public:
         void sem(Semaphore * sem) {
             _sem = sem;
         }
-        bool* id() {
-            return _id;
+        short frame_id() {
+            return _frame_id;
+        }
+        void frame_id(short id) {
+            _frame_id = id;
+        }
+        bool* status() {
+            return _status;
         }
         Address from() {
             return _from;
@@ -187,7 +205,8 @@ protected:
     static const unsigned int NIC_MTU = 1500;
     static const unsigned int Bolinha_MTU = NIC_MTU - sizeof(Header);
     short _using_port = -1;
-
+    short _packet_count = 0;
+    Thread _packet_monitor;
 };
 
 // bool Bolinha_Protocol::_ports[] = {0};
