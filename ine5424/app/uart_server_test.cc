1e397783872e1e6375a85657fb185886eaa61c99
gstvob <gustavo.borgesfr@gmail.com>
2019-11-18 20:41:46 -0300

    Adjusting to use COM1

--- ine5424/app/uart_server_test.cc
+++ ine5424/app/uart_server_test.cc
@@ -10,7 +10,7 @@ int main()
 
     cout << "UART test\n" << endl;
 
-    UART uart(0, 115200, 8, 0, 1);
+    UART uart(1, 115200, 8, 0, 1);
 
     cout << "Loopback transmission test (conf = 115200 8N1):";
     uart.loopback(false);
